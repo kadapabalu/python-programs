107a24382d21568a00f5f82cad6e52cac062c91d
kadapabalu <kadapabalu002@gmail.com>
2025-12-20 07:39:08 +0530

    Create the longest consecutive sequence.py

---
+++ the longest consecutive sequence.py
@@ -0,0 +1,11 @@
+def longest_consecutive(nums):
+    num_set = set(nums)
+    longest = 0
+    for num in num_set:
+        if num - 1 not in num_set:
+            length = 1
+            while num + length in num_set:
+                length += 1
+            longest = max(longest, length)
+    return longest
+print(longest_consecutive([100, 4, 200, 1, 3, 2]))
