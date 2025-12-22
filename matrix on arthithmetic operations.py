c04e47e8e6d774481de0eec06d991b554ac56ed5
kadapabalu <kadapabalu002@gmail.com>
2025-12-22 13:23:15 +0530

    Create matrix on arthithmetic operations.py

---
+++ matrix on arthithmetic operations.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array([[1,2,3],[4,5,6],[7,8,9]])
+b=np.array([[7,8,9],[4,5,6],[1,2,3]])
+add=a+b
+sub=a-b
+mul=a*b
+print("addition=", add)
+print("subtraction=", sub)
+print("multiplication=", mul)
