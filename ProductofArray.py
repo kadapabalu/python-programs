bd05c59be22253a3421b31b34b4f8d27230ac887
kadapabalu <kadapabalu002@gmail.com>
2025-12-23 20:40:26 +0530

    Create ProductofArray.py

---
+++ ProductofArray.py
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4]
+n = len(arr)
+left = [1] * n
+right = [1] * n
+for i in range(1, n):
+    left[i] = left[i - 1] * arr[i - 1]
+for i in range(n - 2, -1, -1):
+    right[i] = right[i + 1] * arr[i + 1]
+result = [left[i] * right[i] for i in range(n)]
+print("Result:", result)
