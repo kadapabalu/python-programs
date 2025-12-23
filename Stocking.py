2924d121d1fd8e28e968d302deea4aeab61e0c54
kadapabalu <kadapabalu002@gmail.com>
2025-12-23 20:36:55 +0530

    Create Stocking.py

---
+++ Stocking.py
@@ -0,0 +1,7 @@
+prices = [7, 1, 5, 3, 6, 4]
+min_price = prices[0]
+max_profit = 0
+for price in prices:
+    min_price = min(min_price, price)
+    max_profit = max(max_profit, price - min_price)
+print("Maximum Profit:", max_profit)
