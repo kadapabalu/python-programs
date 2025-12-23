3edce12cf7493c25020eae67eb62be9c5eee9942
kadapabalu <kadapabalu002@gmail.com>
2025-12-23 20:24:48 +0530

    Create LoginSystem.py

---
+++ LoginSystem.py
@@ -0,0 +1,8 @@
+username="admin"
+password="1234"
+u=input("username: ")
+p=input("password: ")
+if u==username and p==password:
+    print("Login successful")
+else:
+    print("Invalid credentials")
