ae05fe1e9cf8786a3a5ba9c9ab506851ae4033a6
kadapabalu <kadapabalu002@gmail.com>
2025-12-23 20:31:24 +0530

    Create Kadane’sAlgorithm.py

---
+++ Kadane’sAlgorithm.py
@@ -0,0 +1,6 @@
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+max_sum = current_sum = arr[0]
+for num in arr[1:]:
+    current_sum = max(num, current_sum + num)
+    max_sum = max(max_sum, current_sum)
+print("Maximum Subarray Sum:", max_sum)
